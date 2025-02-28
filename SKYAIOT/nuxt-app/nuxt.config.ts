dee305381ec73b8fb19e3024640ac3f8585541d5
Xiang511 <toby91511@gmail.com>
2025-02-28 13:17:03 +0800

    feat: Add owl.carousel and Jquery

--- SKYAIOT/nuxt-app/nuxt.config.ts
+++ SKYAIOT/nuxt-app/nuxt.config.ts
@@ -4,7 +4,13 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: [
     '~/public/css/global.css',
-    'bootstrap/dist/css/bootstrap.min.css'
-  ]
+    'bootstrap/dist/css/bootstrap.min.css',
+    'owl.carousel/dist/assets/owl.carousel.css',
+    'owl.carousel/dist/assets/owl.theme.default.css'
+  ],
+  plugins: [
+    { src: '~/plugins/jquery_3.7.1_jquery.min.js', mode: 'client' },
+    { src: '~/plugins/owl-carousel.js', mode: 'client' }
+  ],
   
 })
